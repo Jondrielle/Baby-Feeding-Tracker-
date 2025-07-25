b34bf4ed425c38163ca7f0e69763689c8139c145
Jondri <jondriellewilson@gmail.com>
2025-07-25 12:07:34 -0500

    Create feed delete route and delete all feed entries route

--- backend/feeds.py
+++ backend/feeds.py
@@ -1,7 +1,7 @@
 from schemas import FeedResponse, FeedEntry
 from models import Feed 
-from fastapi import APIRouter,Depends,HTTPException
-from sqlmodel import Session
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
 from database import get_session
 from typing import List
 
@@ -10,15 +10,15 @@ router = APIRouter()
 
 @router.post("/feeding", response_model= FeedResponse, status_code = 201)
 def create_feed(feed: FeedEntry, session: Session = Depends(get_session)):
-	try:
-		db_feed = Feed(**feed.dict())
-		session.add(db_feed)
-		session.commit()
-		session.refresh(db_feed)
-		return db_feed
-	except Exception as e:
-		session.rollback()
-		raise HTTPException(status_code = 400, detail = f"Error with creating feed entry: {e}")
+    try:
+        db_feed = Feed(**feed.dict())
+        session.add(db_feed)
+        session.commit()
+        session.refresh(db_feed)
+        return db_feed
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code = 400, detail = f"Error with creating feed entry: {e}")
 
 @router.get("/feedings", response_model=List[FeedResponse])
 def get_all_feedings(session: Session = Depends(get_session)):
@@ -29,3 +29,25 @@ def get_all_feedings(session: Session = Depends(get_session)):
         return feeds
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Server error: {str(e)}")
+
+@router.delete("/feedings/{feed_id}", status_code=204)
+def delete_feed_entry(feed_id: int, session: Session = Depends(get_session)):
+    feed = session.get(Feed, feed_id)
+    if not feed:
+        raise HTTPException(status_code=404, detail="No feed entries found")
+    
+    session.delete(feed)
+    session.commit()
+    return {"detail": "Feed entry deleted"}
+
+@router.delete("/feedings/", status_code=204)
+def delete_all_feed_entries(session: Session = Depends(get_session)):
+    feedings = session.exec(select(Feed)).all()
+    if not feedings:
+        raise HTTPException(status_code=404, detail="No feed entries to delete")
+    
+    for feed in feedings:
+        session.delete(feed)
+
+    session.commit()
+    return {"detail": "All feed entries deleted"}
