c8029d1fdacbe003ffe8552dc5998277865b3a6b
Jondri <jondriellewilson@gmail.com>
2025-07-27 21:41:09 -0500

    Setting up frontend boilerplate

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from database import create_db_and_tables
 from feeds import router as feeds_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.on_event("startup")
 def startup():
---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>Baby Feeding Tracker</h1>
+    <button @click= "fetchItem">Create Feed Entry</button>
+  </div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+
+const items = ref([])
+
+onMounted(async () => {
+  const res = await fetch('http://localhost:8000/feedings')
+  items.value = await res.json()
+})
+
+async function fetchItem(){
+  const res = await fetch('http://localhost:8000/feeding/')
+  items.value = await res.json()
+}
+
+
+</script>
+
+<style>
+/* Optional global styles */
+</style>
