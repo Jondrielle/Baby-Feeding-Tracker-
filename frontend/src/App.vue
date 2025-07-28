69023ea07a5f8acf1f4109e0955630934b72cbdc
Jondri <jondriellewilson@gmail.com>
2025-07-27 22:19:38 -0500

    Set up feeding list to display on screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,25 +2,49 @@
 <template>
   <div>
     <h1>Baby Feeding Tracker</h1>
-    <button @click= "fetchItem">Create Feed Entry</button>
+    <button @click= "createFeeding">Create Feed Entry</button>
+
+    <!-- List of Feedings -->
+    <ul>
+      <li v-for="feeding in feedings" :key="feeding.id">
+        {{feeding.time}} - 
+        {{feeding.amount_ml !== null && feeding.amount_ml !== undefined ? feeding.amount_ml : 'N/A'}} ml 
+        {{feeding.amount_oz !== null && feeding.amount_oz !== undefined ? feeding.amount_oz : 'N/A'}} oz - {{feeding.method}} - {{feeding.notes || '' }} 
+      </li>
+    </ul>
+
+
   </div>
 </template>
 
 <script setup>
 import {ref, onMounted} from 'vue'
 
-const items = ref([])
+const feedings = ref([])
+
+const feedingEntry = ref({
+  time:'',
+  amount_ml:'',
+  amount_oz:'',
+  method:'',
+  notes:''
+})
 
 onMounted(async () => {
   const res = await fetch('http://localhost:8000/feedings')
-  items.value = await res.json()
+  feedings.value = await res.json()
 })
 
-async function fetchItem(){
-  const res = await fetch('http://localhost:8000/feeding/')
-  items.value = await res.json()
+async function createFeeding(){
+  const res = await fetch('http://localhost:8000/feeding', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(feedingEntry.value)
+  })
+  const newFeeding = await res.json()
+  feedings.value.push(newFeeding)
+  feedingData.value = { time: '', amount_ml: '', amount_oz: '', method: '', notes: '' }
 }
-
 
 </script>
 
